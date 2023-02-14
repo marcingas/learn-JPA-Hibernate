0337edfa9b7355ec55e8fc2b0e932588240a1ab3
Marcin <gasiorek.mail@gmail.com>
2023-02-14 13:20:00 +0100

    Initial commit- create database url

---
+++ src/test/java/dev/Marcin/learnJPAHibernate/LearnJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Marcin.learnJPAHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
