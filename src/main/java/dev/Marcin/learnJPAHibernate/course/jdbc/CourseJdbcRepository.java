d17e8912bafb5fe9e5b19369c60ea0b0e34e0edc
Marcin <gasiorek.mail@gmail.com>
2023-02-14 13:54:28 +0100

    inserting data to table course using Spring JDBC

---
+++ src/main/java/dev/Marcin/learnJPAHibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,19 @@
+package dev.Marcin.learnJPAHibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
+    private static String INSERT_QUERY =
+            """
+            insert into course (id, name, author)\s
+            values (2, 'Learn Java vol 2', 'dev master Marcin')
+            """;
+    public void insert(){
+        springJdbcTemplate.update(INSERT_QUERY);
+    }
+}
