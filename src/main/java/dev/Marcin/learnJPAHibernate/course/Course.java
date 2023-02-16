292779a109ba995aeef8cab83c295e70e8e20921
Marcin <gasiorek.mail@gmail.com>
2023-02-16 13:20:03 +0100

    new class JpaRepository prepare to add retrieve remove items with JPA

--- src/main/java/dev/Marcin/learnJPAHibernate/course/Course.java
+++ src/main/java/dev/Marcin/learnJPAHibernate/course/Course.java
@@ -1,6 +1,12 @@
 package dev.Marcin.learnJPAHibernate.course;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Course {
+    @Id
     private long id;
     private String name;
     private String author;
---
+++ src/main/java/dev/Marcin/learnJPAHibernate/course/Jpa/CourseJpaRepository.java
@@ -0,0 +1,10 @@
+package dev.Marcin.learnJPAHibernate.course.Jpa;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJpaRepository {
+    private EntityManager entityManager;
+
+}
